595d82ced1df40c8143abcc670238a26fc5927ca
MadsMGrin <business.madsmadsen@gmail.com>
2023-05-15 16:02:17 +0200

    quality of life

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import {FormsModule} from "@angular/forms";
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import {MatSnackBarModule} from "@angular/material/snack-bar";
         BrowserModule,
         FormsModule,
         BrowserAnimationsModule,
-      MatSnackBarModule
+        MatSnackBarModule,
+        MatCardModule,
+        MatButtonModule
     ],
   providers: [],
   bootstrap: [AppComponent]
